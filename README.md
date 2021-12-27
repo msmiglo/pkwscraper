5484f054944221712332554a8a955be65406cc13
msmiglo <55049009+msmiglo@users.noreply.github.com>
2021-12-27 06:18:58 +0100

    002 internet adapter cache (#8)
    002 class Downloader written with tests, all tests passing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pkwscraper
-Scraping election results in Poland
+Scraping, processing and visualizing election results in Poland.
+
+This project uses no third-party DB engine. I've created simple wrapper to simulate NoSQL DB engine by wrapping csv files reader/writer. Tables are stored in the csv file each. The query syntax and API is inspired by MongoDB. This is to learn and thereafter show my deeper understaning of DataBase underlying logic and performance-related challenges.
+
+Project is split into 3 parts:
+- downloading raw data from PKW website,
+- processing them to obtain useful and easily accessible database,
+- visualizing desired results of elections on map of Poland.
+
+It also includes API for writing custom scripts. That will allow users to visualize other direct results or some self-defined complex expresions on results values.
---
+++ pkwscraper/lib/downloader.py
@@ -0,0 +1,78 @@
+
+import os
+import requests
+
+
+BASE_URL_DICT = {
+    2011: "https://wybory2011.pkw.gov.pl",
+    2015: "https://parlament2015.pkw.gov.pl",
+    2019: "https://sejmsenat2019.pkw.gov.pl/sejmsenat2019",
+}
+ACCEPTED_EXTENSIONS = [
+    ".csv",
+    ".xls",
+    ".xlsx",
+    ".htm",
+    ".html",
+    ".blob",
+]
+
+
+class Downloader:
+    def __init__(self, year, directory):
+        """
+        year: int/str - year of Sejm elections to scrape
+        directory: str - local directory for caching files
+        """
+        # choose base URL for given elections
+        self.__base_url = BASE_URL_DICT[int(year)]
+
+        # check if cache directory exists
+        if not os.path.isdir(directory):
+            raise IOError("The caching directory does not exist!")
+        self.__local_directory = directory
+
+    def download(self, relative_url, force=False):
+        # convert url to file path
+        if not relative_url.startswith("/"):
+            raise ValueError('Relative URL path should start with "/".')
+        filename = self._convert_filename(relative_url)
+        filepath = os.path.join(self.__local_directory, filename)
+
+        # check cache
+        if not force and os.path.exists(filepath):
+            file_content = self._load_file(filepath)
+            return file_content
+
+        # download file from Internet
+        url = self.__base_url + relative_url
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise ConnectionError(
+                f'Server did not return a requested resource. '
+                f'Reason: "{response.reason}".')
+
+        # cache and return the file content
+        file_content = response.content
+        self._save_file(file_content, filepath)
+        return file_content
+
+    @staticmethod
+    def _convert_filename(relative_url):
+        if relative_url == "/":
+            relative_url = "/index.html"
+        if not any(relative_url.endswith(extension) for extension in ACCEPTED_EXTENSIONS):
+            raise ValueError("Unsupported type of requested file.")
+        filename = relative_url.replace("/", "_").strip("_")
+        return filename
+
+    @staticmethod
+    def _save_file(content, filepath):
+        with open(filepath, "wb") as f:
+            f.write(content)
+
+    @staticmethod
+    def _load_file(filepath):
+        with open(filepath, "rb") as f:
+            content = f.read()
+        return content
