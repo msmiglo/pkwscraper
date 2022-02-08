09f0efffe8a464e7c665b6d016e0a312b29c47d4
Maciej Śmigielski <user@server.fake>
2022-02-08 15:19:17 +0100

    039 implemented Controller class loosely, need to refactor

--- pkwscraper/scripts/visualize_sejm_2015_territories.py
+++ pkwscraper/scripts/visualize_sejm_2015_territories.py
@@ -11,6 +11,7 @@ from matplotlib.collections import PatchCollection
 from matplotlib.patches import PathPatch, Polygon
 from matplotlib.path import Path
 
+from pkwscraper.lib.controller import Controller
 from pkwscraper.lib.dbdriver import DbDriver
 from pkwscraper.lib.region import Region
 from pkwscraper.lib.utilities import get_parent_code
@@ -183,8 +184,154 @@ class TerritoryVisualizer:
         print(f"max too many candidates percentage: {vis.maxs[1]}")
         print(f"max too many candidates absolute: {vis.maxs[2]}")
 
+    def visualize_2a(self):
+        # make colormap
+        '''
+        def colormap(values):
+            # unpack measures
+            invalid_percent, too_many_candidates_percent, \
+                too_many_absolute = values
+
+            # determine color components
+            red = too_many_candidates_percent
+            green = 1 - invalid_percent
+            blue = min(1, max(0, invalid_percent - red))
+            alpha = 0.82
+
+            # return color
+            return [red, green, blue, alpha]
+        '''
+        colormap = Colormap(color_data={
+            (0., 0.): (0.0, 1.0, 0.0, 0.82),
+            (0., 1.): (1.0, 1.0, 0.0, 0.82),
+            (1., 0.): (0.0, 0.0, 1.0, 0.82),
+            (1., 1.): (1.0, 0.0, 0.0, 0.82),
+            #(0.5, 0.5): (0.5, 0.5, 0.0, 0.82),
+            (0.5, 1.0): (1.0, 0.5, 0.0, 0.82),
+            (0.5, 0.0): (0.0, 0.5, 0.5, 0.82),
+            (1.0, 0.5): (0.5, 0.0, 0.5, 0.82),
+            (0.0, 0.5): (0.5, 1.0, 0.0, 0.82),
+        })
+        def function(db):
+            return 1, 0
+
+
+
+
+            # read communes data from DB
+            gminy_data = self.db["gminy"].find({}, fields=["_id", "geo"])
+            gminy = {
+                gmina_id: {
+                    "region": Region.from_json(geo),
+                    "voters": 0,
+                    "ballots_valid": 0,
+                    "votes_invalid": 0,
+                    "invalid_2_candidates": 0,
+                    "votes_valid": 0,
+                }
+                for gmina_id, geo in gminy_data
+            }
+
+            # read protocoles data from polling districts from DB
+            protocoles = self.db["protokoły"].find(
+                query={},
+                fields=["obwod", "voters", "ballots_valid",
+                        "votes_invalid", "invalid_2_candidates", "votes_valid"]
+            )
+
+            # iterate over protocoles
+            for protocole_record in protocoles:
+                # unzip data
+                obwod_id = protocole_record[0]
+                voters = protocole_record[1]
+                ballots_valid = protocole_record[2]
+                votes_invalid = protocole_record[3]
+                invalid_2_candidates = protocole_record[4]
+                votes_valid = protocole_record[5]
+
+                # get commune entry
+                gmina_id = self.db["obwody"][obwod_id]["gmina"]
+                gmina = gminy[gmina_id]
+
+                # add votes for commune entry
+                gmina["voters"] += voters
+                gmina["ballots_valid"] += ballots_valid
+                gmina["votes_invalid"] += votes_invalid
+                gmina["invalid_2_candidates"] += invalid_2_candidates
+                gmina["votes_valid"] += votes_valid
+
+            return gminy
+
+        ctrl = Controller(
+            ("Sejm", 2015), function, colormap, granularity="communes",
+            outlines_granularity="constituencies", normalization=False,
+            output_filename="głosy_nieważne.png"
+        )
+        ctrl.run()
+        return
+
+
+
+
+        # get constituencies
+        okregi_regions = [
+            Region.from_json(geo)
+            for geo
+            in self.db["okręgi"].find(
+                query={}, fields="geo")
+        ]
+
+        # preprocess gminy
+        gminy_data = self.get_invalid()
+        gminy = [
+            gmina
+            for gmina in gminy_data.values()
+            if not gmina["region"].is_empty()
+        ]
+        gminy_regions = [gmina["region"] for gmina in gminy]
+        gminy_values = []
+
+        for gmina in gminy:
+            # calculate measures
+            voters = gmina["voters"]
+            ballots_valid = gmina["ballots_valid"]
+            votes_invalid = gmina["votes_invalid"]
+            invalid_2_candidates = gmina["invalid_2_candidates"]
+            votes_valid = gmina["votes_valid"]
+
+            invalid_percent = votes_invalid / ballots_valid
+            too_many_candidates_percent = invalid_2_candidates / votes_invalid
+            too_many_absolute = invalid_2_candidates / ballots_valid
+
+            # add element
+            gminy_values.append([
+                invalid_percent,
+                too_many_candidates_percent,
+                too_many_absolute
+            ])
+
+
+        # make visualizer
+        vis = Visualizer(
+            regions=gminy_regions,
+            values=gminy_values,
+            colormap=colormap,
+            contours=okregi_regions,
+            normalization_range=[(0, 1), (0, 1), (0, 1)]
+        )
+        vis.normalize_values()
+        vis.render_colors()
+        vis.prepare()
+        vis.show()
+
+        # show max values
+        print(f"max invalid votes percentage: {vis.maxs[0]}")
+        print(f"max too many candidates percentage: {vis.maxs[1]}")
+        print(f"max too many candidates absolute: {vis.maxs[2]}")
+
 
 if __name__ == "__main__":
     ter_vis = TerritoryVisualizer()
-    ter_vis.visualize()
-    ter_vis.visualize_2()
+    #ter_vis.visualize()
+    #ter_vis.visualize_2()
+    ter_vis.visualize_2a()
